7df4e6a884887aee7f8e5dd99e639fa654ab9af9
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-10-20 14:17:29 -0300

    Update tabelaclientes.sql

--- scripts/tabelaclientes.sql
+++ scripts/tabelaclientes.sql
@@ -63,15 +63,26 @@ values (14, 'Jeferson', null, null, '1983-07-01', 'M', null, 'Brasileiro', null,
 insert into cliente (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
 values (15, 'Jessica', null, null, null, 'F', 'Estudante', null, null, null, null, null, 'União da Vitória', 'PR');
 
+select * from cliente;
 
+select nome, data_nascimento from cliente;
 
+select nome, data_nascimento as "Data de Nascimento" from cliente;
 
+select 'CPF: ' || cpf || ' RG: ' || rg as "CPF e RG" from cliente;
 
+select * from cliente limit 3;
 
+select nome, data_nascimento from cliente where data_nascimento > '2000-01-01';
 
+select nome from cliente where nome like 'C%';
 
+select nome from cliente where nome like '%c%';
 
+select nome, data_nascimento from cliente where data_nascimento between '1990-01-01' and '1998-01-01';
 
+select nome, rg from cliente where rg is null;
 
+select nome from cliente order by nome;
 
-
+select nome from cliente order by nome desc;
